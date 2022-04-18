7cbbff5ec410485e775ef170a9c0f5268a1212a8
Banging12 <96330757+Banging12@users.noreply.github.com>
2022-04-18 11:21:07 +0100

    Initialize cmcewen/les-snapshot

---
+++ consensus/istanbul/core/roundchange.go
@@ -0,0 +1,404 @@
+// Copyright 2017 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package core
+
+import (
+	"fmt"
+	"math/big"
+	"sort"
+	"strings"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/consensus/istanbul"
+)
+
+// sendRoundChange sends the ROUND CHANGE message with the given round
+func (c *core) sendRoundChange(round *big.Int) {
+	logger := c.newLogger("func", "sendRoundChange", "target_round", round)
+
+	if c.current.View().Round.Cmp(round) >= 0 {
+		logger.Warn("Cannot send round change for previous round")
+		return
+	}
+
+	msg, err := c.buildRoundChangeMsg(round)
+	if err != nil {
+		logger.Error("Could not build round change message", "err", msg)
+		return
+	}
+
+	c.broadcast(msg)
+}
+
+// sendRoundChange sends a ROUND CHANGE message for the current round back to a single address
+func (c *core) sendRoundChangeAgain(addr common.Address) {
+	logger := c.newLogger("func", "sendRoundChange", "desired_round", c.current.DesiredRound(), "to", addr)
+
+	msg, err := c.buildRoundChangeMsg(c.current.DesiredRound())
+	if err != nil {
+		logger.Error("Could not build round change message", "err", err)
+		return
+	}
+
+	c.unicast(msg, addr)
+}
+
+// buildRoundChangeMsg creates a round change msg for the given round
+func (c *core) buildRoundChangeMsg(round *big.Int) (*istanbul.Message, error) {
+	nextView := &istanbul.View{
+		Round:    new(big.Int).Set(round),
+		Sequence: new(big.Int).Set(c.current.View().Sequence),
+	}
+
+	rc := &istanbul.RoundChange{
+		View:                nextView,
+		PreparedCertificate: c.current.PreparedCertificate(),
+	}
+
+	payload, err := Encode(rc)
+	if err != nil {
+		return nil, err
+	}
+
+	return &istanbul.Message{
+		Code: istanbul.MsgRoundChange,
+		Msg:  payload,
+	}, nil
+}
+
+func (c *core) handleRoundChangeCertificate(proposal istanbul.Subject, roundChangeCertificate istanbul.RoundChangeCertificate) error {
+	logger := c.newLogger("func", "handleRoundChangeCertificate", "proposal_round", proposal.View.Round, "proposal_seq", proposal.View.Sequence, "proposal_digest", proposal.Digest.String())
+
+	if len(roundChangeCertificate.RoundChangeMessages) > c.current.ValidatorSet().Size() || len(roundChangeCertificate.RoundChangeMessages) < c.current.ValidatorSet().MinQuorumSize() {
+		return errInvalidRoundChangeCertificateNumMsgs
+	}
+
+	maxRound := big.NewInt(-1)
+	preferredDigest := common.Hash{}
+	seen := make(map[common.Address]bool)
+	decodedMessages := make([]istanbul.RoundChange, len(roundChangeCertificate.RoundChangeMessages))
+	for i := range roundChangeCertificate.RoundChangeMessages {
+		// use a different variable each time since we'll store a pointer to the variable
+		message := roundChangeCertificate.RoundChangeMessages[i]
+
+		// Verify message signed by a validator
+		data, err := message.PayloadNoSig()
+		if err != nil {
+			return err
+		}
+
+		signer, err := c.validateFn(data, message.Signature)
+		if err != nil {
+			return err
+		}
+
+		if signer != message.Address {
+			return errInvalidRoundChangeCertificateMsgSignature
+		}
+
+		// Check for duplicate ROUND CHANGE messages
+		if seen[signer] {
+			return errInvalidRoundChangeCertificateDuplicate
+		}
+		seen[signer] = true
+
+		// Check that the message is a ROUND CHANGE message
+		if istanbul.MsgRoundChange != message.Code {
+			return errInvalidRoundChangeCertificateMsgCode
+		}
+
+		var roundChange *istanbul.RoundChange
+		if err := message.Decode(&roundChange); err != nil {
+			logger.Warn("Failed to decode ROUND CHANGE in certificate", "err", err)
+			return err
+		}
+		msg_logger := logger.New("msg_round", roundChange.View.Round, "msg_seq", roundChange.View.Sequence)
+
+		// Verify ROUND CHANGE message is for a proper view
+		if roundChange.View.Cmp(proposal.View) != 0 || roundChange.View.Round.Cmp(c.current.DesiredRound()) < 0 {
+			msg_logger.Error("Round change in certificate for wrong view", "err", err)
+			return errInvalidRoundChangeCertificateMsgView
+		}
+
+		if roundChange.HasPreparedCertificate() {
+			msg_logger.Trace("Round change message has prepared certificate")
+			if err := c.verifyPreparedCertificate(roundChange.PreparedCertificate); err != nil {
+				return err
+			}
+			// We must use the proposal in the prepared certificate with the highest round number. (See OSDI 99, Section 4.4)
+			// Older prepared certificates may be generated, but if no node committed, there is no guarantee that
+			// it will be the next pre-prepare. If one node committed, that block is guaranteed (by quorum intersection)
+			// to be the next pre-prepare. That (higher view) prepared cert should override older perpared certs for
+			// blocks that were not committed.
+			// Also reject round change messages where the prepared view is greater than the round change view.
+			preparedView := roundChange.PreparedCertificate.View()
+			msg_logger = msg_logger.New("prepared_round", preparedView.Round, "prepared_seq", preparedView.Sequence)
+			if preparedView == nil || preparedView.Round.Cmp(proposal.View.Round) > 0 {
+				return errInvalidRoundChangeViewMismatch
+			} else if preparedView.Round.Cmp(maxRound) > 0 {
+				msg_logger.Trace("Prepared certificate is latest in round change certificate")
+				maxRound = preparedView.Round
+				preferredDigest = roundChange.PreparedCertificate.Proposal.Hash()
+			}
+		}
+
+		decodedMessages[i] = *roundChange
+		// TODO(joshua): startNewRound needs these round change messages to generate a
+		// round change certificate even if this node is not the next proposer
+		c.roundChangeSet.Add(roundChange.View.Round, &message)
+	}
+
+	if maxRound.Cmp(big.NewInt(-1)) > 0 && proposal.Digest != preferredDigest {
+		return errInvalidPreparedCertificateDigestMismatch
+	}
+
+	// May have already moved to this round based on quorum round change messages.
+	logger.Trace("Trying to move to round change certificate's round", "target round", proposal.View.Round)
+
+	return c.startNewRound(proposal.View.Round)
+}
+
+func (c *core) handleRoundChange(msg *istanbul.Message) error {
+	logger := c.newLogger("func", "handleRoundChange", "tag", "handleMsg", "from", msg.Address)
+
+	// Decode ROUND CHANGE message
+	var rc *istanbul.RoundChange
+	if err := msg.Decode(&rc); err != nil {
+		logger.Info("Failed to decode ROUND CHANGE", "err", err)
+		return errInvalidMessage
+	}
+	logger = logger.New("msg_round", rc.View.Round, "msg_seq", rc.View.Sequence)
+
+	// Must be same sequence and future round.
+	err := c.checkMessage(istanbul.MsgRoundChange, rc.View)
+
+	// If the RC message is for the current sequence but a prior round, help the sender fast forward
+	// by sending back to it (not broadcasting) a round change message for our desired round.
+	if err == errOldMessage && rc.View.Sequence.Cmp(c.current.Sequence()) == 0 {
+		logger.Trace("Sending round change for desired round to node with a previous desired round", "msg_round", rc.View.Round)
+		c.sendRoundChangeAgain(msg.Address)
+		return nil
+	} else if err != nil {
+		logger.Debug("Check round change message failed", "err", err)
+		return err
+	}
+
+	// Verify the PREPARED certificate if present.
+	if rc.HasPreparedCertificate() {
+		if err := c.verifyPreparedCertificate(rc.PreparedCertificate); err != nil {
+			return err
+		}
+		preparedCertView := rc.PreparedCertificate.View()
+		if preparedCertView == nil || preparedCertView.Round.Cmp(rc.View.Round) > 0 {
+			return errInvalidRoundChangeViewMismatch
+		}
+	}
+
+	roundView := rc.View
+
+	// Add the ROUND CHANGE message to its message set.
+	if err := c.roundChangeSet.Add(roundView.Round, msg); err != nil {
+		logger.Warn("Failed to add round change message", "roundView", roundView, "err", err)
+		return err
+	}
+
+	// Skip to the highest round we know F+1 (one honest validator) is at, but
+	// don't start a round until we have a quorum who want to start a given round.
+	ffRound := c.roundChangeSet.MaxRound(c.current.ValidatorSet().F() + 1)
+	quorumRound := c.roundChangeSet.MaxOnOneRound(c.current.ValidatorSet().MinQuorumSize())
+	logger = logger.New("ffRound", ffRound, "quorumRound", quorumRound)
+	logger.Trace("Got round change message", "rcs", c.roundChangeSet.String())
+	// On f+1 round changes we send a round change and wait for the next round if we haven't done so already
+	// On quorum round change messages we go to the next round immediately.
+	if quorumRound != nil && quorumRound.Cmp(c.current.DesiredRound()) >= 0 {
+		logger.Debug("Got quorum round change messages, starting new round.")
+		return c.startNewRound(quorumRound)
+	} else if ffRound != nil {
+		logger.Debug("Got f+1 round change messages, sending own round change message and waiting for next round.")
+		c.waitForDesiredRound(ffRound)
+	}
+
+	return nil
+}
+
+// ----------------------------------------------------------------------------
+
+func newRoundChangeSet(valSet istanbul.ValidatorSet) *roundChangeSet {
+	return &roundChangeSet{
+		validatorSet:      valSet,
+		msgsForRound:      make(map[uint64]MessageSet),
+		latestRoundForVal: make(map[common.Address]uint64),
+		mu:                new(sync.Mutex),
+	}
+}
+
+type roundChangeSet struct {
+	validatorSet      istanbul.ValidatorSet
+	msgsForRound      map[uint64]MessageSet
+	latestRoundForVal map[common.Address]uint64
+	mu                *sync.Mutex
+}
+
+// Add adds the round and message into round change set
+func (rcs *roundChangeSet) Add(r *big.Int, msg *istanbul.Message) error {
+	rcs.mu.Lock()
+	defer rcs.mu.Unlock()
+
+	src := msg.Address
+	round := r.Uint64()
+
+	if prevLatestRound, ok := rcs.latestRoundForVal[src]; ok {
+		if prevLatestRound > round {
+			// Reject as we have an RC for a later round from this validator.
+			return errOldMessage
+		} else if prevLatestRound < round {
+			// Already got an RC for an earlier round from this validator.
+			// Forget that and remember this.
+			if rcs.msgsForRound[prevLatestRound] != nil {
+				rcs.msgsForRound[prevLatestRound].Remove(src)
+				if rcs.msgsForRound[prevLatestRound].Size() == 0 {
+					delete(rcs.msgsForRound, prevLatestRound)
+				}
+			}
+		}
+	}
+
+	rcs.latestRoundForVal[src] = round
+
+	if rcs.msgsForRound[round] == nil {
+		rcs.msgsForRound[round] = newMessageSet(rcs.validatorSet)
+	}
+	return rcs.msgsForRound[round].Add(msg)
+}
+
+// Clear deletes the messages with smaller round
+func (rcs *roundChangeSet) Clear(round *big.Int) {
+	rcs.mu.Lock()
+	defer rcs.mu.Unlock()
+
+	for k, rms := range rcs.msgsForRound {
+		if rms.Size() == 0 || k < round.Uint64() {
+			for _, msg := range rms.Values() {
+				if _, ok := rcs.latestRoundForVal[msg.Address]; ok {
+					delete(rcs.latestRoundForVal, msg.Address)
+				}
+			}
+			delete(rcs.msgsForRound, k)
+		}
+	}
+}
+
+// MaxRound returns the max round which the number of messages is equal or larger than num
+func (rcs *roundChangeSet) MaxRound(num int) *big.Int {
+	rcs.mu.Lock()
+	defer rcs.mu.Unlock()
+
+	// Sort rounds descending
+	var sortedRounds []uint64
+	for r := range rcs.msgsForRound {
+		sortedRounds = append(sortedRounds, r)
+	}
+	sort.Slice(sortedRounds, func(i, j int) bool { return sortedRounds[i] > sortedRounds[j] })
+
+	acc := 0
+	for _, r := range sortedRounds {
+		rms := rcs.msgsForRound[r]
+		acc += rms.Size()
+		if acc >= num {
+			return new(big.Int).SetUint64(r)
+		}
+	}
+
+	return nil
+}
+
+// MaxOnOneRound returns the max round which the number of messages is >= num
+func (rcs *roundChangeSet) MaxOnOneRound(num int) *big.Int {
+	rcs.mu.Lock()
+	defer rcs.mu.Unlock()
+
+	// Sort rounds descending
+	var sortedRounds []uint64
+	for r := range rcs.msgsForRound {
+		sortedRounds = append(sortedRounds, r)
+	}
+	sort.Slice(sortedRounds, func(i, j int) bool { return sortedRounds[i] > sortedRounds[j] })
+
+	for _, r := range sortedRounds {
+		rms := rcs.msgsForRound[r]
+		if rms.Size() >= num {
+			return new(big.Int).SetUint64(r)
+		}
+	}
+	return nil
+}
+
+func (rcs *roundChangeSet) String() string {
+	rcs.mu.Lock()
+	defer rcs.mu.Unlock()
+
+	// Sort rounds descending
+	var sortedRounds []uint64
+	for r := range rcs.msgsForRound {
+		sortedRounds = append(sortedRounds, r)
+	}
+	sort.Slice(sortedRounds, func(i, j int) bool { return sortedRounds[i] > sortedRounds[j] })
+
+	modeRound := uint64(0)
+	modeRoundSize := 0
+	msgsForRoundStr := make([]string, 0, len(sortedRounds))
+	for _, r := range sortedRounds {
+		rms := rcs.msgsForRound[r]
+		if rms.Size() > modeRoundSize {
+			modeRound = r
+			modeRoundSize = rms.Size()
+		}
+		msgsForRoundStr = append(msgsForRoundStr, fmt.Sprintf("%v: %v", r, rms.String()))
+	}
+
+	latestRoundForValStr := make([]string, 0, len(rcs.latestRoundForVal))
+	for addr, r := range rcs.latestRoundForVal {
+		latestRoundForValStr = append(latestRoundForValStr, fmt.Sprintf("%v: %v", addr.String(), r))
+	}
+
+	return fmt.Sprintf("RCS len=%v mode_round=%v mode_round_len=%v unique_rounds=<%v> %v",
+		len(rcs.latestRoundForVal),
+		modeRound,
+		modeRoundSize,
+		len(rcs.msgsForRound),
+		strings.Join(msgsForRoundStr, ", "))
+}
+
+// Gets a round change certificate for a specific round.
+func (rcs *roundChangeSet) getCertificate(r *big.Int, quorumSize int) (istanbul.RoundChangeCertificate, error) {
+	rcs.mu.Lock()
+	defer rcs.mu.Unlock()
+
+	round := r.Uint64()
+	if rcs.msgsForRound[round] != nil && rcs.msgsForRound[round].Size() >= quorumSize {
+		messages := make([]istanbul.Message, rcs.msgsForRound[round].Size())
+		for i, message := range rcs.msgsForRound[round].Values() {
+			messages[i] = *message
+		}
+		return istanbul.RoundChangeCertificate{
+			RoundChangeMessages: messages,
+		}, nil
+	} else {
+		return istanbul.RoundChangeCertificate{}, errFailedCreateRoundChangeCertificate
+	}
+}
---
+++ consensus/istanbul/types.go
@@ -0,0 +1,456 @@
+// Copyright 2017 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package istanbul
+
+import (
+	"fmt"
+	"io"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+// UptimeEntry contains the uptime score of a validator during an epoch as well as the
+// last block they signed on
+type UptimeEntry struct {
+	ScoreTally      uint64
+	LastSignedBlock uint64
+}
+
+func (u *UptimeEntry) String() string {
+	return fmt.Sprintf("UptimeEntry { scoreTally: %v, lastBlock: %v}", u.ScoreTally, u.LastSignedBlock)
+}
+
+// Uptime contains the latest block for which uptime metrics were accounted for. It also contains
+// an array of Entries where the `i`th entry represents the uptime statistics of the `i`th validator
+// in the validator set for that epoch
+type Uptime struct {
+	LatestBlock uint64
+	Entries     []UptimeEntry
+}
+
+// Proposal supports retrieving height and serialized block to be used during Istanbul consensus.
+type Proposal interface {
+	// Number retrieves the sequence number of this proposal.
+	Number() *big.Int
+
+	Header() *types.Header
+
+	// Hash retrieves the hash of this block
+	Hash() common.Hash
+
+	// ParentHash retrieves the hash of this block's parent
+	ParentHash() common.Hash
+
+	EncodeRLP(w io.Writer) error
+
+	DecodeRLP(s *rlp.Stream) error
+}
+
+type Request struct {
+	Proposal Proposal
+}
+
+// EncodeRLP serializes b into the Ethereum RLP format.
+func (b *Request) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, []interface{}{b.Proposal})
+}
+
+// DecodeRLP implements rlp.Decoder, and load the consensus fields from a RLP stream.
+func (b *Request) DecodeRLP(s *rlp.Stream) error {
+	var request struct {
+		Proposal *types.Block
+	}
+
+	if err := s.Decode(&request); err != nil {
+		return err
+	}
+
+	b.Proposal = request.Proposal
+	return nil
+}
+
+// View includes a round number and a sequence number.
+// Sequence is the block number we'd like to commit.
+// Each round has a number and is composed by 3 steps: preprepare, prepare and commit.
+//
+// If the given block is not accepted by validators, a round change will occur
+// and the validators start a new round with round+1.
+type View struct {
+	Round    *big.Int
+	Sequence *big.Int
+}
+
+// EncodeRLP serializes b into the Ethereum RLP format.
+func (v *View) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, []interface{}{v.Round, v.Sequence})
+}
+
+// DecodeRLP implements rlp.Decoder, and load the consensus fields from a RLP stream.
+func (v *View) DecodeRLP(s *rlp.Stream) error {
+	var view struct {
+		Round    *big.Int
+		Sequence *big.Int
+	}
+
+	if err := s.Decode(&view); err != nil {
+		return err
+	}
+	v.Round, v.Sequence = view.Round, view.Sequence
+	return nil
+}
+
+func (v *View) String() string {
+	return fmt.Sprintf("{Round: %d, Sequence: %d}", v.Round.Uint64(), v.Sequence.Uint64())
+}
+
+// Cmp compares v and y and returns:
+//   -1 if v <  y
+//    0 if v == y
+//   +1 if v >  y
+func (v *View) Cmp(y *View) int {
+	if v.Sequence.Cmp(y.Sequence) != 0 {
+		return v.Sequence.Cmp(y.Sequence)
+	}
+	if v.Round.Cmp(y.Round) != 0 {
+		return v.Round.Cmp(y.Round)
+	}
+	return 0
+}
+
+type RoundChangeCertificate struct {
+	RoundChangeMessages []Message
+}
+
+func (b *RoundChangeCertificate) IsEmpty() bool {
+	return len(b.RoundChangeMessages) == 0
+}
+
+// EncodeRLP serializes b into the Ethereum RLP format.
+func (b *RoundChangeCertificate) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, []interface{}{b.RoundChangeMessages})
+}
+
+// DecodeRLP implements rlp.Decoder, and load the consensus fields from a RLP stream.
+func (b *RoundChangeCertificate) DecodeRLP(s *rlp.Stream) error {
+	var roundChangeCertificate struct {
+		RoundChangeMessages []Message
+	}
+
+	if err := s.Decode(&roundChangeCertificate); err != nil {
+		return err
+	}
+	b.RoundChangeMessages = roundChangeCertificate.RoundChangeMessages
+
+	return nil
+}
+
+type Preprepare struct {
+	View                   *View
+	Proposal               Proposal
+	RoundChangeCertificate RoundChangeCertificate
+}
+
+func (b *Preprepare) HasRoundChangeCertificate() bool {
+	return !b.RoundChangeCertificate.IsEmpty()
+}
+
+// EncodeRLP serializes b into the Ethereum RLP format.
+func (b *Preprepare) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, []interface{}{b.View, b.Proposal, &b.RoundChangeCertificate})
+}
+
+// DecodeRLP implements rlp.Decoder, and load the consensus fields from a RLP stream.
+func (b *Preprepare) DecodeRLP(s *rlp.Stream) error {
+	var preprepare struct {
+		View                   *View
+		Proposal               *types.Block
+		RoundChangeCertificate RoundChangeCertificate
+	}
+
+	if err := s.Decode(&preprepare); err != nil {
+		return err
+	}
+	b.View, b.Proposal, b.RoundChangeCertificate = preprepare.View, preprepare.Proposal, preprepare.RoundChangeCertificate
+
+	return nil
+}
+
+type PreparedCertificate struct {
+	Proposal                Proposal
+	PrepareOrCommitMessages []Message
+}
+
+func EmptyPreparedCertificate() PreparedCertificate {
+	emptyHeader := &types.Header{
+		Difficulty: big.NewInt(0),
+		Number:     big.NewInt(0),
+		GasLimit:   0,
+		GasUsed:    0,
+		Time:       big.NewInt(0),
+	}
+	block := &types.Block{}
+	block = block.WithRandomness(&types.EmptyRandomness)
+
+	return PreparedCertificate{
+		Proposal:                block.WithSeal(emptyHeader),
+		PrepareOrCommitMessages: []Message{},
+	}
+}
+
+func (b *PreparedCertificate) IsEmpty() bool {
+	return len(b.PrepareOrCommitMessages) == 0
+}
+
+func (b *PreparedCertificate) View() *View {
+	if b.IsEmpty() {
+		return nil
+	}
+	msg := b.PrepareOrCommitMessages[0]
+	var s *Subject
+	err := msg.Decode(&s)
+	if err != nil {
+		return nil
+	}
+	return s.View
+}
+
+// EncodeRLP serializes b into the Ethereum RLP format.
+func (b *PreparedCertificate) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, []interface{}{b.Proposal, b.PrepareOrCommitMessages})
+}
+
+// DecodeRLP implements rlp.Decoder, and load the consensus fields from a RLP stream.
+func (b *PreparedCertificate) DecodeRLP(s *rlp.Stream) error {
+	var preparedCertificate struct {
+		Proposal                *types.Block
+		PrepareOrCommitMessages []Message
+	}
+
+	if err := s.Decode(&preparedCertificate); err != nil {
+		return err
+	}
+
+	b.Proposal, b.PrepareOrCommitMessages = preparedCertificate.Proposal, preparedCertificate.PrepareOrCommitMessages
+	return nil
+}
+
+type RoundChange struct {
+	View                *View
+	PreparedCertificate PreparedCertificate
+}
+
+func (b *RoundChange) HasPreparedCertificate() bool {
+	return !b.PreparedCertificate.IsEmpty()
+}
+
+// EncodeRLP serializes b into the Ethereum RLP format.
+func (b *RoundChange) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, []interface{}{b.View, &b.PreparedCertificate})
+}
+
+// DecodeRLP implements rlp.Decoder, and load the consensus fields from a RLP stream.
+func (b *RoundChange) DecodeRLP(s *rlp.Stream) error {
+	var roundChange struct {
+		View                *View
+		PreparedCertificate PreparedCertificate
+	}
+
+	if err := s.Decode(&roundChange); err != nil {
+		return err
+	}
+	b.View, b.PreparedCertificate = roundChange.View, roundChange.PreparedCertificate
+	return nil
+}
+
+type Subject struct {
+	View   *View
+	Digest common.Hash
+}
+
+// EncodeRLP serializes b into the Ethereum RLP format.
+func (b *Subject) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, []interface{}{b.View, b.Digest})
+}
+
+// DecodeRLP implements rlp.Decoder, and load the consensus fields from a RLP stream.
+func (b *Subject) DecodeRLP(s *rlp.Stream) error {
+	var subject struct {
+		View   *View
+		Digest common.Hash
+	}
+
+	if err := s.Decode(&subject); err != nil {
+		return err
+	}
+	b.View, b.Digest = subject.View, subject.Digest
+	return nil
+}
+
+func (b *Subject) String() string {
+	return fmt.Sprintf("{View: %v, Digest: %v}", b.View, b.Digest.String())
+}
+
+type CommittedSubject struct {
+	Subject       *Subject
+	CommittedSeal []byte
+}
+
+// EncodeRLP serializes b into the Ethereum RLP format.
+func (cs *CommittedSubject) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, []interface{}{cs.Subject, cs.CommittedSeal})
+}
+
+// DecodeRLP implements rlp.Decoder, and load the consensus fields from a RLP stream.
+func (cs *CommittedSubject) DecodeRLP(s *rlp.Stream) error {
+	var committedSubject struct {
+		Subject       *Subject
+		CommittedSeal []byte
+	}
+
+	if err := s.Decode(&committedSubject); err != nil {
+		return err
+	}
+	cs.Subject, cs.CommittedSeal = committedSubject.Subject, committedSubject.CommittedSeal
+	return nil
+}
+
+type ForwardMessage struct {
+	Msg           []byte
+	DestAddresses []common.Address
+}
+
+// EncodeRLP serializes fm into the Ethereum RLP format.
+func (fm *ForwardMessage) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, []interface{}{fm.Msg, fm.DestAddresses})
+}
+
+// DecodeRLP implements rlp.Decoder, and load the consensus fields from a RLP stream.
+func (fm *ForwardMessage) DecodeRLP(s *rlp.Stream) error {
+	var forwardMessage struct {
+		Msg           []byte
+		DestAddresses []common.Address
+	}
+
+	if err := s.Decode(&forwardMessage); err != nil {
+		return err
+	}
+	fm.Msg, fm.DestAddresses = forwardMessage.Msg, forwardMessage.DestAddresses
+	return nil
+}
+
+const (
+	MsgPreprepare uint64 = iota
+	MsgPrepare
+	MsgCommit
+	MsgRoundChange
+)
+
+type Message struct {
+	Code      uint64
+	Msg       []byte
+	Address   common.Address // The sender address
+	Signature []byte         // Signature of the Message using the private key associated with the "Address" field
+}
+
+// ==============================================
+//
+// define the functions that needs to be provided for rlp Encoder/Decoder.
+
+// EncodeRLP serializes m into the Ethereum RLP format.
+func (m *Message) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, []interface{}{m.Code, m.Msg, m.Address, m.Signature})
+}
+
+// DecodeRLP implements rlp.Decoder, and load the consensus fields from a RLP stream.
+func (m *Message) DecodeRLP(s *rlp.Stream) error {
+	var msg struct {
+		Code      uint64
+		Msg       []byte
+		Address   common.Address
+		Signature []byte
+	}
+
+	if err := s.Decode(&msg); err != nil {
+		return err
+	}
+	m.Code, m.Msg, m.Address, m.Signature = msg.Code, msg.Msg, msg.Address, msg.Signature
+	return nil
+}
+
+// ==============================================
+//
+// define the functions that needs to be provided for core.
+
+func (m *Message) Sign(signingFn func(data []byte) ([]byte, error)) error {
+	// Construct and encode a message with no signature
+	payloadNoSig, err := m.PayloadNoSig()
+	if err != nil {
+		return err
+	}
+	m.Signature, err = signingFn(payloadNoSig)
+	return err
+}
+
+func (m *Message) FromPayload(b []byte, validateFn func([]byte, []byte) (common.Address, error)) error {
+	// Decode Message
+	err := rlp.DecodeBytes(b, &m)
+	if err != nil {
+		return err
+	}
+
+	// Validate message (on a message without Signature)
+	if validateFn != nil {
+		var payload []byte
+		payload, err = m.PayloadNoSig()
+		if err != nil {
+			return err
+		}
+
+		signed_val_addr, err := validateFn(payload, m.Signature)
+		if err != nil {
+			return err
+		}
+		if signed_val_addr != m.Address {
+			return ErrInvalidSigner
+		}
+	}
+	return nil
+}
+
+func (m *Message) Payload() ([]byte, error) {
+	return rlp.EncodeToBytes(m)
+}
+
+func (m *Message) PayloadNoSig() ([]byte, error) {
+	return rlp.EncodeToBytes(&Message{
+		Code:      m.Code,
+		Msg:       m.Msg,
+		Address:   m.Address,
+		Signature: []byte{},
+	})
+}
+
+func (m *Message) Decode(val interface{}) error {
+	return rlp.DecodeBytes(m.Msg, val)
+}
+
+func (m *Message) String() string {
+	return fmt.Sprintf("{Code: %v, Address: %v}", m.Code, m.Address.String())
+}
