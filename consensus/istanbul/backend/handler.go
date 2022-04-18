e126c05705c69abc32fefa361009fa832b7bfc02
Banging12 <96330757+Banging12@users.noreply.github.com>
2022-04-18 11:24:15 +0100

    Initialize release/baklava-phase-3

---
+++ consensus/istanbul/backend/handler.go
@@ -0,0 +1,276 @@
+// Copyright 2017 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package backend
+
+import (
+	"errors"
+	"reflect"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/consensus"
+	"github.com/ethereum/go-ethereum/consensus/istanbul"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+	"github.com/ethereum/go-ethereum/p2p"
+	lru "github.com/hashicorp/golang-lru"
+)
+
+var (
+	// errDecodeFailed is returned when decode message fails
+	errDecodeFailed = errors.New("fail to decode istanbul message")
+)
+
+// If you want to add a code, you need to increment the Lengths Array size!
+const (
+	istanbulConsensusMsg = 0x11
+	// TODO:  Support sending multiple announce messages withone one message
+	istanbulAnnounceMsg            = 0x12
+	istanbulValEnodesShareMsg      = 0x13
+	istanbulFwdMsg                 = 0x14
+	istanbulDelegateSign           = 0x15
+	istanbulGetAnnouncesMsg        = 0x16
+	istanbulGetAnnounceVersionsMsg = 0x17
+	istanbulAnnounceVersionsMsg    = 0x18
+)
+
+func (sb *Backend) isIstanbulMsg(msg p2p.Msg) bool {
+	return msg.Code >= istanbulConsensusMsg && msg.Code <= istanbulAnnounceVersionsMsg
+}
+
+type announceMsgHandler func(consensus.Peer, []byte) error
+
+// HandleMsg implements consensus.Handler.HandleMsg
+func (sb *Backend) HandleMsg(addr common.Address, msg p2p.Msg, peer consensus.Peer) (bool, error) {
+	sb.coreMu.Lock()
+	defer sb.coreMu.Unlock()
+
+	logger := sb.logger.New("func", "HandleMsg", "m", msg, "peer", peer)
+
+	logger.Trace("Handling a message")
+
+	if sb.isIstanbulMsg(msg) {
+		if (!sb.coreStarted && !sb.config.Proxy) && (msg.Code == istanbulConsensusMsg) {
+			return true, istanbul.ErrStoppedEngine
+		}
+
+		var data []byte
+		if err := msg.Decode(&data); err != nil {
+			if err == errUnauthorized {
+				logger.Debug("Failed to decode message payload", "err", err)
+			} else {
+				logger.Error("Failed to decode message payload", "err", err)
+			}
+			return true, errDecodeFailed
+		}
+
+		if msg.Code == istanbulDelegateSign {
+			if sb.shouldHandleDelegateSign() {
+				go sb.delegateSignFeed.Send(istanbul.MessageEvent{Payload: data})
+				return true, nil
+			}
+
+			return true, errors.New("No proxy or proxied validator found")
+		}
+
+		// Only use the recent messages and known messages cache for the
+		// Announce message.  That is the only message that is gossiped.
+		if msg.Code == istanbulAnnounceMsg {
+			hash := istanbul.RLPHash(data)
+
+			// Mark peer's message
+			ms, ok := sb.peerRecentMessages.Get(addr)
+			var m *lru.ARCCache
+			if ok {
+				m, _ = ms.(*lru.ARCCache)
+			} else {
+				m, _ = lru.NewARC(inmemoryMessages)
+				sb.peerRecentMessages.Add(addr, m)
+			}
+			m.Add(hash, true)
+
+			// Mark self known message
+			if _, ok := sb.selfRecentMessages.Get(hash); ok {
+				return true, nil
+			}
+			sb.selfRecentMessages.Add(hash, true)
+		}
+
+		if msg.Code == istanbulConsensusMsg {
+			err := sb.handleConsensusMsg(peer, data)
+			return true, err
+		} else if msg.Code == istanbulFwdMsg {
+			err := sb.handleFwdMsg(peer, data)
+			return true, err
+		} else if announceHandlerFunc, ok := sb.istanbulAnnounceMsgHandlers[msg.Code]; ok { // Note that the valEnodeShare message is handled here as well
+			go announceHandlerFunc(peer, data)
+			return true, nil
+		}
+
+		// If we got here, then that means that there is an istanbul message type that we
+		// don't handle, and hence a bug in the code.
+		logger.Crit("Unhandled istanbul message type")
+		return false, nil
+	}
+	return false, nil
+}
+
+// Handle an incoming consensus msg
+func (sb *Backend) handleConsensusMsg(peer consensus.Peer, payload []byte) error {
+	if sb.config.Proxy {
+		// Verify that this message is not from the proxied peer
+		if reflect.DeepEqual(peer, sb.proxiedPeer) {
+			sb.logger.Warn("Got a consensus message from the proxied validator.  Ignoring it")
+			return nil
+		}
+
+		// Need to forward the message to the proxied validator
+		sb.logger.Trace("Forwarding consensus message to proxied validator")
+		if sb.proxiedPeer != nil {
+			go sb.proxiedPeer.Send(istanbulConsensusMsg, payload)
+		}
+	} else { // The case when this node is a validator
+		go sb.istanbulEventMux.Post(istanbul.MessageEvent{
+			Payload: payload,
+		})
+	}
+
+	return nil
+}
+
+// Handle an incoming forward msg
+func (sb *Backend) handleFwdMsg(peer consensus.Peer, payload []byte) error {
+	// Ignore the message if this node it not a proxy
+	if !sb.config.Proxy {
+		sb.logger.Warn("Got a forward consensus message and this node is not a proxy.  Ignoring it")
+		return nil
+	}
+
+	// Verify that it's coming from the proxied peer
+	if !reflect.DeepEqual(peer, sb.proxiedPeer) {
+		sb.logger.Warn("Got a forward consensus message from a non proxied valiator.  Ignoring it")
+		return nil
+	}
+
+	istMsg := new(istanbul.Message)
+
+	// An Istanbul FwdMsg doesn't have a signature since it's coming from a trusted peer and
+	// the wrapped message is already signed by the proxied validator.
+	if err := istMsg.FromPayload(payload, nil); err != nil {
+		sb.logger.Error("Failed to decode message from payload", "err", err)
+		return err
+	}
+
+	var fwdMsg *istanbul.ForwardMessage
+	err := istMsg.Decode(&fwdMsg)
+	if err != nil {
+		sb.logger.Error("Failed to decode a ForwardMessage", "err", err)
+		return err
+	}
+
+	sb.logger.Debug("Forwarding a consensus message")
+	go sb.Multicast(fwdMsg.DestAddresses, fwdMsg.Msg, istanbulConsensusMsg)
+	return nil
+}
+
+func (sb *Backend) shouldHandleDelegateSign() bool {
+	return sb.IsProxy() || sb.IsProxiedValidator()
+}
+
+// SubscribeNewDelegateSignEvent subscribes a channel to any new delegate sign messages
+func (sb *Backend) SubscribeNewDelegateSignEvent(ch chan<- istanbul.MessageEvent) event.Subscription {
+	return sb.delegateSignScope.Track(sb.delegateSignFeed.Subscribe(ch))
+}
+
+// SetBroadcaster implements consensus.Handler.SetBroadcaster
+func (sb *Backend) SetBroadcaster(broadcaster consensus.Broadcaster) {
+	sb.broadcaster = broadcaster
+}
+
+// SetP2PServer implements consensus.Handler.SetP2PServer
+func (sb *Backend) SetP2PServer(p2pserver consensus.P2PServer) {
+	sb.p2pserver = p2pserver
+}
+
+// This function is called by miner/worker.go whenever it's mainLoop gets a newWork event.
+func (sb *Backend) NewWork() error {
+	sb.coreMu.RLock()
+	defer sb.coreMu.RUnlock()
+	if !sb.coreStarted {
+		return istanbul.ErrStoppedEngine
+	}
+
+	go sb.istanbulEventMux.Post(istanbul.FinalCommittedEvent{})
+	return nil
+}
+
+// This function is called by all nodes.
+// At the end of each epoch, this function will
+//    1)  Output if it is or isn't an elected validator if it has mining turned on.
+//    2)  Refresh the validator connections if it's a proxy or non proxied validator
+func (sb *Backend) NewChainHead(newBlock *types.Block) {
+	if istanbul.IsLastBlockOfEpoch(newBlock.Number().Uint64(), sb.config.Epoch) {
+		sb.coreMu.RLock()
+		defer sb.coreMu.RUnlock()
+
+		valset := sb.getValidators(newBlock.Number().Uint64(), newBlock.Hash())
+
+		// Output whether this validator was or wasn't elected for the
+		// new epoch's validator set
+		if sb.coreStarted {
+			_, val := valset.GetByAddress(sb.ValidatorAddress())
+			sb.logger.Info("Validator Election Results", "address", sb.ValidatorAddress(), "elected", (val != nil), "number", newBlock.Number().Uint64())
+		}
+
+		// If this is a proxy or a non proxied validator and a
+		// new epoch just started, then refresh the validator enode table
+		sb.logger.Trace("At end of epoch and going to refresh validator peers", "new block number", newBlock.Number().Uint64())
+		sb.RefreshValPeers(valset)
+	}
+}
+
+func (sb *Backend) RegisterPeer(peer consensus.Peer, isProxiedPeer bool) {
+	// TODO - For added security, we may want the node keys of the proxied validators to be
+	//        registered with the proxy, and verify that all newly connected proxied peer has
+	//        the correct node key
+
+	sb.logger.Trace("RegisterPeer called", "peer", peer, "isProxiedPeer", isProxiedPeer)
+
+	// Check to see if this connecting peer if a proxied validator
+	if sb.config.Proxy && isProxiedPeer {
+		sb.proxiedPeer = peer
+	} else if sb.config.Proxied {
+		if sb.proxyNode != nil && peer.Node().ID() == sb.proxyNode.node.ID() {
+			sb.proxyNode.peer = peer
+		} else {
+			sb.logger.Error("Unauthorized connected peer to the proxied validator", "peer node", peer.Node().String())
+		}
+	}
+
+	if peer.Version() >= 65 {
+		sb.sendGetAnnounceVersions(peer)
+	}
+}
+
+func (sb *Backend) UnregisterPeer(peer consensus.Peer, isProxiedPeer bool) {
+	if sb.config.Proxy && isProxiedPeer && reflect.DeepEqual(sb.proxiedPeer, peer) {
+		sb.proxiedPeer = nil
+	} else if sb.config.Proxied {
+		if sb.proxyNode != nil && peer.Node().ID() == sb.proxyNode.node.ID() {
+			sb.proxyNode.peer = nil
+		}
+	}
+}
---
+++ consensus/istanbul/backend/pos.go
@@ -0,0 +1,235 @@
+// Copyright 2017 The celo Authors
+// This file is part of the celo library.
+//
+// The celo library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The celo library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the celo library. If not, see <http://www.gnu.org/licenses/>.
+
+package backend
+
+import (
+	"errors"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/consensus/istanbul"
+	"github.com/ethereum/go-ethereum/contract_comm"
+	"github.com/ethereum/go-ethereum/contract_comm/currency"
+	"github.com/ethereum/go-ethereum/contract_comm/election"
+	"github.com/ethereum/go-ethereum/contract_comm/epoch_rewards"
+	"github.com/ethereum/go-ethereum/contract_comm/gold_token"
+	"github.com/ethereum/go-ethereum/contract_comm/validators"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/params"
+)
+
+func (sb *Backend) distributeEpochPaymentsAndRewards(header *types.Header, state *state.StateDB) error {
+	start := time.Now()
+	defer sb.rewardDistributionTimer.UpdateSince(start)
+
+	err := epoch_rewards.UpdateTargetVotingYield(header, state)
+	if err != nil {
+		return err
+	}
+	validatorEpochPayment, totalVoterRewards, err := epoch_rewards.CalculateTargetEpochPaymentAndRewards(header, state)
+	if err != nil {
+		return err
+	}
+	log.Debug("Calculated target epoch payment and rewards", "validatorEpochPayment", validatorEpochPayment, "totalVoterRewards", totalVoterRewards)
+
+	// The validator set that signs off on the last block of the epoch is the one that we need to
+	// iterate over.
+	valSet := sb.GetValidators(big.NewInt(header.Number.Int64()-1), header.ParentHash)
+	if len(valSet) == 0 {
+		err := errors.New("Unable to fetch validator set to update scores and distribute payments and rewards")
+		sb.logger.Error(err.Error())
+		return err
+	}
+
+	uptimes, err := sb.updateValidatorScores(header, state, valSet)
+	if err != nil {
+		return err
+	}
+
+	totalEpochPayments, err := sb.distributeEpochPayments(header, state, valSet, validatorEpochPayment)
+	if err != nil {
+		return err
+	}
+
+	totalEpochRewards, err := sb.distributeEpochRewards(header, state, valSet, totalVoterRewards, uptimes)
+	if err != nil {
+		return err
+	}
+
+	stableTokenAddress, err := contract_comm.GetRegisteredAddress(params.StableTokenRegistryId, header, state)
+	if err != nil {
+		return err
+	}
+
+	totalEpochPaymentsConvertedToGold, err := currency.Convert(totalEpochPayments, stableTokenAddress, nil)
+	if err != nil {
+		return err
+	}
+
+	reserveAddress, err := contract_comm.GetRegisteredAddress(params.ReserveRegistryId, header, state)
+	if err != nil {
+		return err
+	}
+	if reserveAddress != nil {
+		state.AddBalance(*reserveAddress, totalEpochPaymentsConvertedToGold)
+	} else {
+		return errors.New("Unable to fetch reserve address for epoch rewards distribution")
+	}
+
+	return sb.increaseGoldTokenTotalSupply(header, state, big.NewInt(0).Add(totalEpochRewards, totalEpochPaymentsConvertedToGold))
+}
+
+func (sb *Backend) updateValidatorScores(header *types.Header, state *state.StateDB, valSet []istanbul.Validator) ([]*big.Int, error) {
+	epoch := istanbul.GetEpochNumber(header.Number.Uint64(), sb.EpochSize())
+	logger := sb.logger.New("func", "Backend.updateValidatorScores", "blocknum", header.Number.Uint64(), "epoch", epoch, "epochsize", sb.EpochSize(), "window", sb.LookbackWindow())
+	logger.Trace("Updating validator scores")
+
+	// The denominator is the (last block - first block + 1) of the val score tally window
+	denominator := istanbul.GetValScoreTallyLastBlockNumber(epoch, sb.EpochSize()) - istanbul.GetValScoreTallyFirstBlockNumber(epoch, sb.EpochSize(), sb.LookbackWindow()) + 1
+
+	uptimes := make([]*big.Int, 0, len(valSet))
+	for i, entry := range rawdb.ReadAccumulatedEpochUptime(sb.db, epoch).Entries {
+		if i >= len(valSet) {
+			break
+		}
+		val_logger := logger.New("scoreTally", entry.ScoreTally, "denominator", denominator, "index", i, "address", valSet[i].Address())
+
+		if entry.ScoreTally > denominator {
+			val_logger.Error("ScoreTally exceeds max possible")
+			uptimes = append(uptimes, params.Fixidity1)
+			continue
+		}
+
+		numerator := big.NewInt(0).Mul(big.NewInt(int64(entry.ScoreTally)), params.Fixidity1)
+		uptimes = append(uptimes, big.NewInt(0).Div(numerator, big.NewInt(int64(denominator))))
+	}
+
+	if len(uptimes) < len(valSet) {
+		err := fmt.Errorf("%d accumulated uptimes found, cannot update validator scores", len(uptimes))
+		logger.Error(err.Error())
+		return nil, err
+	}
+
+	for i, val := range valSet {
+		val_logger := logger.New("uptime", uptimes[i], "address", val.Address())
+		val_logger.Trace("Updating validator score")
+		err := validators.UpdateValidatorScore(header, state, val.Address(), uptimes[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+	return uptimes, nil
+}
+
+func (sb *Backend) distributeEpochPayments(header *types.Header, state *state.StateDB, valSet []istanbul.Validator, maxPayment *big.Int) (*big.Int, error) {
+	totalEpochPayments := big.NewInt(0)
+	for _, val := range valSet {
+		sb.logger.Debug("Distributing epoch payment for address", "address", val.Address())
+		epochPayment, err := validators.DistributeEpochPayment(header, state, val.Address(), maxPayment)
+		if err != nil {
+			return totalEpochPayments, nil
+		}
+		totalEpochPayments.Add(totalEpochPayments, epochPayment)
+	}
+	return totalEpochPayments, nil
+}
+
+func (sb *Backend) distributeEpochRewards(header *types.Header, state *state.StateDB, valSet []istanbul.Validator, maxTotalRewards *big.Int, uptimes []*big.Int) (*big.Int, error) {
+	totalEpochRewards := big.NewInt(0)
+
+	// Fixed epoch reward to the community fund.
+	// TODO(asa): This should be a fraction of the overall reward to stakers.
+	communityEpochReward := big.NewInt(params.Ether)
+	governanceAddress, err := contract_comm.GetRegisteredAddress(params.GovernanceRegistryId, header, state)
+	if err != nil {
+		return totalEpochRewards, err
+	}
+
+	if governanceAddress != nil {
+		state.AddBalance(*governanceAddress, communityEpochReward)
+		totalEpochRewards.Add(totalEpochRewards, communityEpochReward)
+	}
+
+	// Select groups that elected at least one validator aggregate their uptimes.
+	var groups []common.Address
+	groupUptimes := make(map[common.Address][]*big.Int)
+	groupElectedValidator := make(map[common.Address]bool)
+	for i, val := range valSet {
+		group, err := validators.GetMembershipInLastEpoch(header, state, val.Address())
+		if err != nil {
+			return totalEpochRewards, err
+		}
+		if _, ok := groupElectedValidator[group]; !ok {
+			groups = append(groups, group)
+			sb.logger.Debug("Group elected validator", "group", group.String())
+		}
+		groupElectedValidator[group] = true
+		groupUptimes[group] = append(groupUptimes[group], uptimes[i])
+	}
+
+	electionRewards, err := election.DistributeEpochRewards(header, state, groups, maxTotalRewards, groupUptimes)
+	if err != nil {
+		return totalEpochRewards, err
+	}
+	lockedGoldAddress, err := contract_comm.GetRegisteredAddress(params.LockedGoldRegistryId, header, state)
+	if err != nil {
+		return totalEpochRewards, err
+	}
+	if lockedGoldAddress != nil {
+		state.AddBalance(*lockedGoldAddress, electionRewards)
+		totalEpochRewards.Add(totalEpochRewards, electionRewards)
+	} else {
+		return totalEpochRewards, errors.New("Unable to fetch locked gold address for epoch rewards distribution")
+	}
+	return totalEpochRewards, err
+}
+
+func (sb *Backend) setInitialGoldTokenTotalSupplyIfUnset(header *types.Header, state *state.StateDB) error {
+	totalSupply, err := gold_token.GetTotalSupply(header, state)
+	if err != nil {
+		return err
+	}
+	// totalSupply not yet initialized.
+	if totalSupply.Cmp(common.Big0) == 0 {
+		data, err := sb.db.Get(core.DBGenesisSupplyKey)
+		if err != nil {
+			log.Error("Unable to fetch genesisSupply from db", "err", err)
+			return err
+		}
+		genesisSupply := new(big.Int)
+		genesisSupply.SetBytes(data)
+
+		err = sb.increaseGoldTokenTotalSupply(header, state, genesisSupply)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (sb *Backend) increaseGoldTokenTotalSupply(header *types.Header, state *state.StateDB, increase *big.Int) error {
+	if increase.Cmp(common.Big0) > 0 {
+		return gold_token.IncreaseSupply(header, state, increase)
+	}
+	return nil
+}
