93a55b8137cc12621eda249dd51ff0c2756a3b3f
Banging12 <96330757+Banging12@users.noreply.github.com>
2022-04-18 11:59:53 +0100

    Initialize piersy/evm-caller-refactor

---
+++ contract_comm/evm.go
@@ -0,0 +1,109 @@
+// Copyright 2017 The Celo Authors
+// This file is part of the celo library.
+//
+// The celo library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The celo library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the celo library. If not, see <http://www.gnu.org/licenses/>.
+
+package contract_comm
+
+import (
+	"math/big"
+
+	"github.com/celo-org/celo-blockchain/accounts/abi"
+	"github.com/celo-org/celo-blockchain/common"
+	"github.com/celo-org/celo-blockchain/contract_comm/errors"
+	"github.com/celo-org/celo-blockchain/contracts"
+	"github.com/celo-org/celo-blockchain/core/types"
+	"github.com/celo-org/celo-blockchain/core/vm"
+	"github.com/celo-org/celo-blockchain/log"
+)
+
+var (
+	systemEVMFactory vm.SystemEVMFactory
+)
+
+func SetSystemEVMFactory(_systemEVMFactory vm.SystemEVMFactory) {
+	if systemEVMFactory == nil {
+		log.Trace("Setting the systemEVMFactory Singleton")
+		systemEVMFactory = _systemEVMFactory
+	}
+}
+
+func MustGetCaller(header *types.Header, state vm.StateDB) vm.SystemEVM {
+	caller, err := getCaller(header, state)
+	if err != nil {
+		panic("failed to get caller")
+	}
+	return caller
+}
+
+func getCaller(header *types.Header, state vm.StateDB) (vm.SystemEVM, error) {
+	// Normally, when making an evm call, we should use the current block's state.  However,
+	// there are times (e.g. retrieving the set of validators when an epoch ends) that we need
+	// to call the evm using the currently mined block.  In that case, the header and state params
+	// will be non nil.
+	if systemEVMFactory == nil {
+		return nil, errors.ErrNoInternalEvmHandlerSingleton
+	}
+
+	if header == nil {
+		return systemEVMFactory.NewCurrentHeadSystemEVM()
+	}
+
+	return systemEVMFactory.NewSystemEVM(header, state), nil
+}
+
+func GetRegisteredAddress(registryId common.Hash, header *types.Header, state vm.StateDB) (common.Address, error) {
+	caller, err := getCaller(header, state)
+	if err != nil {
+		return common.ZeroAddress, err
+	}
+	return contracts.GetRegisteredAddress(caller, registryId)
+}
+
+func MakeStaticCall(registryId common.Hash, abi abi.ABI, method string, args []interface{}, returnObj interface{}, gas uint64, header *types.Header, state vm.StateDB) error {
+	caller, err := getCaller(header, state)
+	if err != nil {
+		return err
+	}
+
+	m := contracts.NewRegistryContractMethod(registryId, &abi, method, gas)
+	return m.VMQuery(caller, returnObj, args...)
+
+}
+
+func MakeCall(registryId common.Hash, abi abi.ABI, method string, args []interface{}, returnObj interface{}, gas uint64, value *big.Int, header *types.Header, state vm.StateDB, finaliseState bool) error {
+	caller, err := getCaller(header, state)
+	if err != nil {
+		return err
+	}
+
+	m := contracts.NewRegistryContractMethod(registryId, &abi, method, gas)
+	err = m.VMExecute(caller, returnObj, value, args...)
+
+	if err == nil && finaliseState {
+		state.Finalise(true)
+	}
+
+	return err
+}
+
+func MakeStaticCallWithAddress(contractAddress common.Address, abi abi.ABI, method string, args []interface{}, returnObj interface{}, gas uint64, header *types.Header, state vm.StateDB) error {
+	caller, err := getCaller(header, state)
+	if err != nil {
+		return err
+	}
+
+	m := contracts.NewBoundMethod(contractAddress, &abi, method, gas)
+	return m.VMQuery(caller, returnObj, args...)
+}
---
+++ contracts/contract_call.go
@@ -0,0 +1,120 @@
+package contracts
+
+import (
+	"math/big"
+
+	"github.com/celo-org/celo-blockchain/accounts/abi"
+	"github.com/celo-org/celo-blockchain/common"
+	"github.com/celo-org/celo-blockchain/common/hexutil"
+	"github.com/celo-org/celo-blockchain/core/vm"
+	"github.com/celo-org/celo-blockchain/log"
+)
+
+// Method represents a contract's method
+type Method struct {
+	abi    *abi.ABI
+	method string
+	maxGas uint64
+}
+
+// NewMethod creates a new contract message
+func NewMethod(abi *abi.ABI, method string, maxGas uint64) Method {
+	return Method{
+		abi:    abi,
+		method: method,
+		maxGas: maxGas,
+	}
+}
+
+func (am Method) Bind(contractAddress common.Address) *BoundMethod {
+	return &BoundMethod{
+		Method:         am,
+		resolveAddress: noopResolver(contractAddress),
+	}
+}
+
+// encodeCall will encodes the msg into []byte format for EVM consumption
+func (am Method) encodeCall(args ...interface{}) ([]byte, error) {
+	return am.abi.Pack(am.method, args...)
+}
+
+// decodeResult will decode the result of msg execution into the result parameter
+func (am Method) decodeResult(result interface{}, output []byte) error {
+	if result == nil {
+		return nil
+	}
+	return am.abi.Unpack(result, am.method, output)
+}
+
+func NewBoundMethod(contractAddress common.Address, abi *abi.ABI, methodName string, maxGas uint64) *BoundMethod {
+	return NewMethod(abi, methodName, maxGas).Bind(contractAddress)
+}
+
+func NewRegistryContractMethod(registryId common.Hash, abi *abi.ABI, methodName string, maxGas uint64) *BoundMethod {
+	return &BoundMethod{
+		Method: NewMethod(abi, methodName, maxGas),
+		resolveAddress: func(caller vm.SystemEVM) (common.Address, error) {
+			return resolveAddressForCall(caller, registryId, methodName)
+		},
+	}
+}
+
+type BoundMethod struct {
+	Method
+	resolveAddress func(vm.SystemEVM) (common.Address, error)
+}
+
+func (bm *BoundMethod) VMQuery(caller vm.SystemEVM, result interface{}, args ...interface{}) error {
+	return bm.Query(caller, result, VMAddress, args...)
+}
+
+func (bm *BoundMethod) Query(caller vm.SystemEVM, result interface{}, sender common.Address, args ...interface{}) error {
+	return bm.run(caller, result, true, sender, nil, args...)
+}
+
+func (bm *BoundMethod) VMExecute(caller vm.SystemEVM, result interface{}, value *big.Int, args ...interface{}) error {
+	return bm.run(caller, result, false, VMAddress, value, args...)
+}
+
+func (bm *BoundMethod) Execute(caller vm.SystemEVM, result interface{}, sender common.Address, value *big.Int, args ...interface{}) error {
+	return bm.run(caller, result, false, sender, value, args...)
+}
+
+func (bm *BoundMethod) run(caller vm.SystemEVM, result interface{}, readOnly bool, sender common.Address, value *big.Int, args ...interface{}) error {
+	defer meterExecutionTime(bm.method)()
+
+	contractAddress, err := bm.resolveAddress(caller)
+	if err != nil {
+		return err
+	}
+
+	logger := log.New("to", contractAddress, "method", bm.method, "args", args, "maxgas", bm.maxGas)
+
+	input, err := bm.encodeCall(args...)
+	if err != nil {
+		logger.Error("Error invoking evm function: can't encode method arguments", "err", err)
+		return err
+	}
+
+	var output []byte
+	var leftoverGas uint64
+	if readOnly {
+		output, leftoverGas, err = caller.Query(sender, contractAddress, input, bm.maxGas)
+	} else {
+		output, leftoverGas, err = caller.Execute(sender, contractAddress, input, bm.maxGas, value)
+	}
+
+	if err != nil {
+		message, _ := unpackError(output)
+		logger.Error("Error invoking evm function: EVM call failure", "input", hexutil.Encode(input), "err", err, "message", message)
+		return err
+	}
+
+	if err := bm.decodeResult(result, output); err != nil {
+		logger.Error("Error invoking evm function: can't unpack result", "err", err, "gasLeft", leftoverGas)
+		return err
+	}
+
+	logger.Trace("EVM call successful", "input", hexutil.Encode(input), "output", hexutil.Encode(output))
+	return nil
+}
